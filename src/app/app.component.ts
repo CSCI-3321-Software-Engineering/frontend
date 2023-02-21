0e9dd9e4db9698ee1d970a1f1531cc5d9ea5befa
Nathan Early <nearly@trinity.edu>
2023-02-21 00:23:32 -0600

    Creating user service and testing api endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { UserService } from './services/user.service';
+import { take } from 'rxjs/operators/';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'frontend';
+  subscriptions: Array<Subscription> = [];
+
+  constructor(
+    // importing the UserService class from user.service.ts
+    private userService: UserService
+  ) {
+
+    // FOR TESTING API ENDPOINT
+
+    // push our new subscription to our array of subscriptions
+    this.subscriptions.push(
+      this.userService.test().subscribe({
+        // we have two different callbacks depending on success or failure 
+        next: (res) => { console.log(res) },
+        error: (error) => { console.log(error) },
+      })
+    );
+
+  }
+
+  ngOnDestroy(): void {
+    // called whenever component will be garbage collected
+    this.subscriptions.forEach((sub) => {
+      sub.unsubscribe(); // Close all memory leaks
+    })
+  }
 }
