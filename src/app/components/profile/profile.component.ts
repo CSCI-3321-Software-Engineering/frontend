781a07dde4e934d2ff55da79fe52f3a71a7af3fc
Nathan Early <nearly@trinity.edu>
2023-03-30 02:51:26 -0500

    reads from component function call

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from 'src/app/services/database/database.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  public username: String;
+  public email: String;
+  public password: String;
+  public id: String;
+  constructor(
+    private databaseConn: DatabaseService
+  ) { 
+    const obj = this.getUserInfo();
+    this.username = obj.username;
+    this.email = obj.email;
+    this.password = obj.password;
+    this.id = obj.id;
+  }
+
+  getUserInfo() { 
+
+
+
+    // placeholder until we get endpoint up to date
+    return {
+      username: "apple",
+      email: "banana",
+      password: "strawberry",
+      id: "rasberry"
+    }
+
+  } 
 
 }
