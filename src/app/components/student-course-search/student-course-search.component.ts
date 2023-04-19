3c380a38fc8381bc7d3f4df93c8e57e49a58a205
Anthony Lisauckis <alisauck@trinity.edu>
2023-04-19 10:47:23 -0500

    Not Functional but adds Code

--- src/app/components/student-course-search/student-course-search.component.ts
+++ src/app/components/student-course-search/student-course-search.component.ts
@@ -1,5 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject } from '@angular/core';
+import { take } from 'rxjs';
+import { DatabaseService  } from 'src/app/services/database/database.service';
 
 @Component({
   selector: 'app-student-course-search',
@@ -9,9 +11,9 @@ import { Component, Inject } from '@angular/core';
 export class StudentCourseSearchComponent {
   public courses;
   @Inject(DOCUMENT) doc: Document
-  constructor( ) { 
+  constructor(private databaseService: DatabaseService) { 
     this.courses = ["Course1", "Course2", "Course3", "Course4", "Course5", "Course6", "Course7", "Course8", "Course9", "Course10"]
-
+    
   }
 
   search() { 
@@ -22,7 +24,51 @@ export class StudentCourseSearchComponent {
     // Object.assign(dropdowns,{
     //     "search": (this.doc.getElementById("search")!.innerHTML)
     //   })
-    console.log("test")
+    // console.log("test")
+
+    
+    const selectDeptElement = document.querySelector<HTMLSelectElement>('#departments');
+    let selectedDeptIndex = selectDeptElement!.selectedIndex;
+    let selectedDeptOptions = selectDeptElement!.selectedOptions;
+    let selectedDeptValue = selectDeptElement!.options[selectedDeptIndex!].value;
+
+    
+    const selectHoursElement = document.querySelector<HTMLSelectElement>('#hours');
+    let selectedHoursIndex = selectHoursElement!.selectedIndex;
+    let selectedHoursOptions = selectHoursElement!.selectedOptions;
+    let selectedHoursValue = selectHoursElement!.options[selectedHoursIndex!].value;
+
+    
+    const selectLevelElement = document.querySelector<HTMLSelectElement>('#level');
+    let selectedLevelIndex = selectLevelElement!.selectedIndex;
+    let selectedLevelOptions = selectLevelElement!.selectedOptions;
+    let selectedLevelValue = selectLevelElement!.options[selectedLevelIndex!].value;
+
+    
+    const selectPrereqElement = document.querySelector<HTMLSelectElement>('#prereqs');
+    let selectedPrereqIndex = selectPrereqElement!.selectedIndex;
+    let selectedPrereqOptions = selectPrereqElement!.selectedOptions;
+    let selectedPrereqValue = selectPrereqElement!.options[selectedPrereqIndex!].value;
+
+    
+    const selectDaysElement = document.querySelector<HTMLSelectElement>('#days');
+    let selectedDaysIndex = selectDaysElement!.selectedIndex;
+    let selectedDaysOptions = selectDaysElement!.selectedOptions;
+    let selectedDaysValue = selectDaysElement!.options[selectedDaysIndex!].value;
+
+    
+    const selectPathwaysElement = document.querySelector<HTMLSelectElement>('#pathways');
+    let selectedPathwaysIndex = selectPathwaysElement!.selectedIndex;
+    let selectedPathwaysOptions = selectPathwaysElement!.selectedOptions;
+    let selectedPathwaysValue = selectPathwaysElement!.options[selectedPathwaysIndex!].value;
+
+    let selectedFilters = [selectedDeptValue, selectedHoursValue, selectedLevelValue, selectedPrereqValue, selectedDaysValue, selectedPathwaysValue]
+
+    
+    this.databaseService.searchCourses(selectedFilters)
+      .pipe(take(1)).subscribe(res => {
+        res.response
+      })
 
     // ids.forEach(id => {
     //   console.log(id);
